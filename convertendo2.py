ff43a3401b632b7e670670ecb30b74d929b68b56
JulioCesarAlcantara <julio.cal93@gmail.com>
2017-10-25 17:47:58 +0000

    convertendo2

---
+++ convertendo2.py
@@ -0,0 +1,56 @@
+import binascii
+
+
+def digito_para_array(numero):
+    array = []
+    for digito in numero:
+        array.append(int(digito))
+
+#     return array
+#
+#
+# def convertToHEX(array2):
+    array1 = []
+    for digito in range(len(array)) :
+        array1.append(hex(array[digito]))
+
+    return array1
+
+def convertToInteger(array3):
+    array = []
+
+    for num in range(len(array3)):
+        array.append(int(array3[num]))
+
+#     return  array
+#
+# def convetToStringUnionInteger(array4):
+    array1 = []
+
+    for val in range(len(array)):
+        array1.append(str(array[val]))
+
+
+    aux = ""
+    for v in range(len(array1)):
+        string = str(aux) + str(array1[v])
+        aux = string
+
+    return  string
+
+#a = 528374123412
+
+#b = digito_para_array(a)
+#print(b)
+
+#c = convertToInteger(b)
+#print(c)
+
+#
+# d = convertToInteger(c)
+# print(d)
+#
+# e = convetToStringUnionInteger(d)
+# print(e)
+
+# print( bytearray.fromhex("7061756c").decode())
