d7463b73c4d4581e1e4b35087c134de638c2407f
JulioCesarAlcantara <julio.cal93@gmail.com>
2017-10-29 22:56:39 -0200

    reader.py

--- reader.py
+++ reader.py
@@ -70,7 +70,7 @@ def startLeitura():
                 things = Things()
                 array.append(things.search_things_by_num2 (numero))
 
-                MIFAREReader.MFRC522_StopCrypto1 ()
+                # MIFAREReader.MFRC522_StopCrypto1 ()
                 # thingsRead = json.dumps(para_dict(array))
 
                 # try:
