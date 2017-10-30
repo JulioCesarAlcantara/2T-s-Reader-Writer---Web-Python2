4864aa6cb07a2feaea2a142716738a44f672d874
JulioCesarAlcantara <julio.cal93@gmail.com>
2017-10-29 22:44:55 -0200

    asdfasd

--- reader.py
+++ reader.py
@@ -68,14 +68,10 @@ def startLeitura():
 
 
                 things = Things()
-
-
                 array.append(things.search_things_by_num2 (numero))
 
                 MIFAREReader.MFRC522_StopCrypto1 ()
                 # thingsRead = json.dumps(para_dict(array))
-
-
 
                 # try:
                 #     arquivo = open ('listRead.json', "w")
