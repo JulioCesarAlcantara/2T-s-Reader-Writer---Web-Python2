43e195f2a41287aabe973b3be9f408c3de9b313b
JulioCesarAlcantara <julio.cal93@gmail.com>
2017-11-01 14:48:48 -0200

    asdf

--- reader.py
+++ reader.py
@@ -65,17 +65,17 @@ def startLeitura():
             print (uid)
 
             # Check if authenticated
-            if status == MIFAREReader.MI_OK:
-                numero = MIFAREReader.MFRC522_Read (sectorBlock)
+            # if status == MIFAREReader.MI_OK:
+            numero = MIFAREReader.MFRC522_Read (sectorBlock)
 
 
-                things = Things()
+            things = Things()
 
 
-                array.append(things.search_things_by_num2 (numero))
+            array.append(things.search_things_by_num2 (numero))
 
-                MIFAREReader.MFRC522_StopCrypto1 ()
-                # thingsRead = json.dumps(para_dict(array))
+            MIFAREReader.MFRC522_StopCrypto1 ()
+            # thingsRead = json.dumps(para_dict(array))
 
 
 
