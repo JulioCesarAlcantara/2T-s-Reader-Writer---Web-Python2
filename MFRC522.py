2fff09531dafa03a0875ee2b2cdb9b1386ffb9d8
JulioCesarAlcantara <julio.cal93@gmail.com>
2017-11-01 15:17:43 -0200

    asdfas

--- MFRC522.py
+++ MFRC522.py
@@ -353,7 +353,7 @@ class MFRC522:
       numero = convertendo2.convertToInteger(backData)
       print("Numero: ")
       print (numero)
-      yield numero
+      return numero
 #      return dados
 
       # try:
